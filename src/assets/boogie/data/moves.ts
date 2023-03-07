7eef18488ee30162f8726623acb438da5276bf48
Mirko <mirko.pani@treebyte.com>
2023-03-07 22:50:03 +0100

    Gh pages test2

--- src/assets/boogie/data/moves.ts
+++ src/assets/boogie/data/moves.ts
@@ -2,9 +2,15 @@ import {DanceMove} from "../../../app/boogie/api/models";
 
 export const MOVES : DanceMove[] = [
     {
-        name : 'Lindy Circle',
-        description: 'Fatta a lezione',
-        labels: ['Boogie Woogie', 'Lindy Hop'],
-        src: 'test'
+        name : 'Spinta da lato',
+        description: 'Ci si tiene per mano, dal lato si tira verso la parte opposta e poi si muove la ballerina fino a dietro',
+        labels: ['Boogie Woogie'],
+        src: 'spintalato'
+    },
+    {
+        name : 'Giro incrociato',
+        description: 'A coppia si gira tenendosi e incrociando le gambe',
+        labels: ['Boogie Woogie'],
+        src: 'incrociato'
     }
 ];
