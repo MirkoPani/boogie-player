db13cd541ff428f2c6a5e4596f9d257304f45af3
Mirko <mirko.pani@treebyte.com>
2023-03-07 22:04:28 +0100

    Auto-generated commit [ci skip]

---
+++ assets/boogie/data/moves.ts
@@ -0,0 +1,10 @@
+import {DanceMove} from "../../../app/boogie/api/models";
+
+export const MOVES : DanceMove[] = [
+    {
+        name : 'Lindy Circle',
+        description: 'Fatta a lezione',
+        labels: ['Boogie Woogie', 'Lindy Hop'],
+        src: 'test'
+    }
+];
