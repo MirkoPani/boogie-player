dd4f8fbdc8cc5a9113b4fa08ef51ade1893b9bd9
Mirko <mirko.pani@treebyte.com>
2023-03-10 11:11:47 +0100

    Auto-generated commit [ci skip]

--- assets/boogie/data/moves.ts
+++ assets/boogie/data/moves.ts
@@ -8,9 +8,27 @@ export const MOVES : DanceMove[] = [
         src: 'spintalato'
     },
     {
-        name : 'Giro incrociato',
-        description: 'A coppia si gira tenendosi e incrociando le gambe',
+        name : 'Giro con gambe incrociato',
+        description: 'La coppia gira attorno ad un cerchio tenendosi e incrociando le gambe',
         labels: ['Boogie Woogie'],
         src: 'incrociato'
+    },
+    {
+        name: 'Casquè con ballerina di spalle',
+        description: 'Casquè fatto girando la ballerina con partenza di mani incrociate.',
+        labels: ['Boogie Woogie'],
+        src: 'casce'
+    },
+    {
+        name: 'Giro e spinta dietro',
+        description: 'Si prendono entrambe le mani, rock step, imbracciatura da dietro e spinta immediata dietro lato della ballerina.',
+        labels: ['Boogie Woogie'],
+        src: 'spintadietro'
+    },
+    {
+        name: 'Blocco spalla imbracciatura',
+        description: 'Si fa imbracciatura, si blocca la spalla e si ruota la ballerina a ritmo.',
+        labels: ['Boogie Woogie'],
+        src: 'bloccoimbracciatura'
     }
 ];
